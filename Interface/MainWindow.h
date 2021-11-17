dfd8c296ab225f14a07260f59886342cf6f77345
cuadriante <adriana@sinapsis.ws>
2021-11-17 00:24:26 -0600

    main window

---
+++ Interface/MainWindow.h
@@ -0,0 +1,33 @@
+//
+// Created by cuadriante on 16/11/21.
+//
+
+#ifndef GALLERYTEC_MAINWINDOW_H
+#define GALLERYTEC_MAINWINDOW_H
+
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QtWidgets>
+#include <QGraphicsItem>
+#include <QGraphicsRectItem>
+#include <QTimer>
+#include <QBrush>
+#include <QDebug>
+#include <QKeyEvent>
+#include <QApplication>
+#include <unistd.h>
+
+using namespace std;
+
+class MainWindow: public QGraphicsView {
+Q_OBJECT
+
+public:
+    QGraphicsScene *scene; /**Graphic scene*/
+    /** Constructor
+     *
+     * @param parent
+     */
+    MainWindow(QWidget* parent=NULL);
+};
+#endif //GALLERYTEC_MAINWINDOW_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <QApplication>
+#include "Interface/MainWindow.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char** argv) {
+
+    QApplication app(argc, argv);
+    MainWindow mainWindow;
+    mainWindow.show();
+
+    return app.exec();
 }
