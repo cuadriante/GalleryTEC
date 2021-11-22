5ef82c68dea261bf206a947d8685ecfd0ab074c4
Gregory Alpízar <gregory.ac@estudiantec.cr>
2021-11-22 14:51:12 -0600

    Change Disk4 name (DIsk4)

--- main.cpp
+++ main.cpp
@@ -10,7 +10,13 @@ int main(int argc, char** argv) {
     raidManager manager;
 
 //    manager.write("Hola, estoy haciendo una prueba");
-
+//    manager.write("5465");
+//    manager.write("perro");
+//    manager.write("...");
+//    manager.write("Volviendo al disk 1");
+//    manager.write("Volviendo al disk 2");
+//    manager.write("Volviendo al disk 3");
+//    manager.write("Volviendo al disk 4");
 //    manager.read();
 
     //QApplication app(argc, argv);
